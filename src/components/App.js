6c7060757310e4caa5cd9b0fb2b95e8946e6519b
JSDM <161076@gmail.com>
2016-10-21 08:50:12 +0545

    refactor to one file for codepen

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import '../style/App.sass';
 
-class App extends Component {
+// page for creating recipe:
+// const createRecipe
+
+// Default page, showing all recipes:
+class RecipeShow extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
+      <div className="card card-block">
+        <img className='card-block' src='http://lorempixel.com/300/180/food' alt='fooditem' />
+        <div className='card-block'>
+          <h3 className='card-title'>Title</h3>
+          <p className='card-text'>lorem</p>
+          <h6>Categories: These are the cats</h6>
+          <button className='btn btn-danger'>Delete</button>
+        </div>
       </div>
     );
   }
 }
 
+var App = React.createClass({
+  render() {
+    return (
+      <div>
+        <RecipeShow />
+      </div>
+    );
+  }
+});
+
 export default App;
+// <img className='card-block' src='...' alt='fooditem' />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import '../style/index.sass';
+import App from './components/App';
+import './style/index.sass';
 
 ReactDOM.render(
   <App />,
